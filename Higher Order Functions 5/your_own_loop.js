c1c4c4563ac481020ccc50d5af9dc407501b8b2f
Reed Zaman <reedzaman98@gmail.com>
2020-12-22 20:26:40 +0600

    added a fizzbuzz with 'loop' in your_own_loop.js

--- Higher Order Functions 5/your_own_loop.js
+++ Higher Order Functions 5/your_own_loop.js
@@ -17,3 +17,9 @@ function loop(value, test, body, update){
 
 // printing 1 to 10
 loop(1, x=> x<11?true:false, x=> console.log(x) , x=> x+=1);
+
+// a one line fizzbuzz function
+let fizzbuzz = x=> !(x%15)?"fizzbuzz": !(x%3)?"fizz": !(x%5)?"buzz": x;
+
+// let's check 1 to 100 for fizzbuzz
+loop(1, x=> x<101?true:false, x=> console.log(fizzbuzz(x)), x=> x+=1 );
