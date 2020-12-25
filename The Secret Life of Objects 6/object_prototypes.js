9d3aa6f2ff5cc7e5d1fe6a446e1f57975dc0c6f7
Reed Zaman <reedzaman98@gmail.com>
2020-12-25 23:19:57 +0600

    some practice with prototypes

---
+++ The Secret Life of Objects 6/object_prototypes.js
@@ -0,0 +1,44 @@
+/*
+.. every object has a default prototype object in the first place
+(Object.prototype)
+*/
+console.log(Object.getPrototypeOf({}) === Object.prototype);
+//-> true
+console.log(Object.getPrototypeOf(Object.prototype));
+//-> null
+// because Object.prototype object has no prototype
+
+
+/*
+.. every object has Object.prototype as default prototype
+.. exceptionally functions have default Function.prototype
+.. and arrays have default Array.prototype
+*/
+console.log(Object.getPrototypeOf(()=>{}) == Function.prototype);
+//-> true
+console.log(Object.getPrototypeOf([]) == Array.prototype)
+//-> true
+
+
+
+/*
+.. using Object.create() to create object with a specific prototype 
+*/
+let rabbit_proto = {
+  hello(){
+    console.log(`${this.type} rabbit says hello...`);
+  }
+}
+let cute_rabbit = Object.create(rabbit_proto);
+cute_rabbit.type = "cute";
+cute_rabbit.hello();
+//-> cute rabbit says hello...
+
+
+
+
+
+
+
+
+console.log("end of viewport..");
