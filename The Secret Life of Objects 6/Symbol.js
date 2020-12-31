9e7b4a9732e7eab677a52cd328f4cb7dbde81f3f
Reed Zaman <reedzaman98@gmail.com>
2020-12-31 22:21:16 +0600

    Symbols more experiments

--- The Secret Life of Objects 6/Symbol.js
+++ The Secret Life of Objects 6/Symbol.js
@@ -24,10 +24,39 @@ console.log(String(reed));
 /*
 .. simle use of symbols
 */
-let sym = Symbol("hello");
+let sym = Symbol();
 human.prototype[sym] = 55;
 console.log(reed[sym]);
-console.log(joy[sym])
+//-> 55
+console.log(joy[sym]);
+//-> 55
+
+
+/*
+.. Symbols are helpful while creating interfaces
+.. for example
+*/
+let  toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function(){
+  return `the length of the array is ${this.length + 1}`;
+}
+console.log([1,2,3,4][toStringSymbol]());
+//-> the length of the array is 5
+
+
+
+/*
+.. another way of including symbol properties to 
+   object expression and classes
+*/
+let obj = {
+  [toStringSymbol](){
+    return `a new way ..`;
+  }
+}
+console.log(obj[toStringSymbol]());
+//-> a new way ..
+
 
 
 
