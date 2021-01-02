16b6262cb4df7c006b9005367fd1bc19abbcc1d2
Reed Zaman <reedzaman98@gmail.com>
2021-01-02 19:03:15 +0600

    running a matrix in a for..of loop

--- The Secret Life of Objects 6/matrix_iterator_interface.js
+++ The Secret Life of Objects 6/matrix_iterator_interface.js
@@ -1,84 +1,109 @@
-class matrix{
-  constructor(height, width){
+/*
+.. a data structure . 2D matrix
+.. TAKES width: width of the matrix (x)
+         height: height of the matrix (y)
+         func: a higher order function that specifies
+         the intial values of the cells
+
+.. HAS   get(): takes x,y cordinates returns that cells value
+         set(): takes x,y cordinates, sets a value to that cell
+         [Symbol.iterator]: returns an iterable hook(object)
+*/
+class matrix {
+  constructor (width, height, func) {
     this.width = width;
     this.height = height;
     this.container = [];
-    for(let x = 0; x<height; x++){
-      for(let y = 0; y<width; y++){
-        this.container[x*width + y] = undefined;
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        this.container[y * width + x] = func(x, y);
       }
     }
   }
-  get(x,y){
-    return this.container[x* this.width + y];
+  get(x, y) {
+    return this.container[y * this.width + x];
   }
-  set(x,y, value){
-    this.container[x* this.width + y] = value;
+  set(x, y, value) {
+    this.container[y * this.width + x] = value;
+  }
+  [Symbol.iterator](){
+    return new matrix_iterator(this);
   }
 }
-let mat = new matrix(6,4);
-mat.set(1,2, 434);
-mat.set(4,2, 44);
-mat.set(3,4, 4);
-mat.set(2,1, 90);
-mat.set(4,3, 3);
-mat.set(1,0, 122);
 
 
-class matrix_iterator{
-  constructor(matrix){
+
+/*
+.. PROVIDES an iterable hook for "for..of" loop
+.. TAKES a 'matrix'
+.. HAS  next(): returns a object with the same format as the object
+                returned by [Symbol.iterator]
+                format, { {an object named value}, a_boolean_named_'done'}
+*/
+class matrix_iterator {
+  constructor(matrix) {
     this.x = 0;
     this.y = 0;
     this.matrix = matrix;
   }
-  
-  next(){
-    if(this.y == this.matrix.height){
-      return {done: true};
+
+  next() {
+    if (this.y == this.matrix.height) {
+      return { done: true };
     }
-    let value = {
-      value: this.matrix.get(this.x, this.y),
+    let hello = this.matrix.get(this.x, this.y);
+    let result = {
+      value: {value: hello},
       done: false
-    }
+    };
     this.x++;
-    if(this.x == this.matrix.width){
+    if (this.x == this.matrix.width) {
       this.y++;
       this.x = 0;
     }
-    return value;
+    return result;
   }
 }
 
 
-let x = new matrix_iterator(mat);
-for(let i=0; i<30; i++){
-  console.log(x.next());
-}
+
+
 /*
-  { value: undefined, done: false }
-  { value: 122, done: false }
-  { value: undefined, done: false }
-  { value: undefined, done: false }
-  { value: undefined, done: false }
-  { value: undefined, done: false }
-  { value: 90, done: false }
-  { value: undefined, done: false }
-
-  ...
-
+.. a new binding name mat of 'matrix' data type
 */
+let mat = new matrix(2, 2, (x, y) => `position ${x} : ${y}`);
 
 
 
 
+/*
+.. running a for..of loop for my compound data structure 'matrix'
+*/
+for ( let {value , done} of mat){
+  console.log(value);
+}
+//-> 'position 0 : 0'
+//-> 'position 1 : 0'
+//-> 'position 0 : 1'
+//-> 'position 1 : 1'
+  
+  
+  
+  
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
-
-
-
-
-
-
-
-
-console.log("end of viewport..");
+console.log('end of viewport..');
