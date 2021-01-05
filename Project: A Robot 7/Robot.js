ecd012e30d1c6954caf4e6eae782b73a1708b919
Reed Zaman <reedzaman98@gmail.com>
2021-01-05 20:39:31 +0600

    creating current state of the robot in the village

--- Project: A Robot 7/Robot.js
+++ Project: A Robot 7/Robot.js
@@ -45,6 +45,42 @@ let road_graph = make_road_graph(roads);
 
 
 
+/*
+.. current state of the robot
+.. consists of: current location
+                parcels to be delivered
+*/
+class village_state {
+  constructor(curr_location, parcels){
+    this.curr_location = curr_location;
+    this.parcels = parcels;
+  }
+  move(destination) {
+    if(this.place === destination) return this;
+    
+    let parcels = this.parcels.map(x => {
+      if(x.place == this.curr_location){
+        return {place: destination, address: x.address};
+      }
+      else {
+        return {place: x.place, address: x.address}
+      }
+    }).filter(x => x.place != x.address);
+    
+    return new village_state(destination, parcels);
+  }
+}
+
+
+
+let memory = undefined;
+
+
+
+
+
+
+
 
 
 
